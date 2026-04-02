1488a4f007bc755e61a2b399067152efc15f13f5
DK <danielccabral1981@gmail.com>
2026-04-01 23:05:30 -0300

    feat: ajustando conect com banco e criando as primeiras rotas de user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Patch, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Prisma } from '../generated/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('')
+  async createUser(@Body() dataUser: Prisma.UserCreateInput) {
+    return this.userService.createUser(dataUser);
+  }
+
+  @Get('')
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Patch('/:id')
+  async editUser(@Param('id') id: string, @Body() dataUser: Prisma.UserUpdateInput) {
+    return this.userService.editUser(id, dataUser);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from '../database/prisma.service';
+import { Prisma, User } from '../generated/client';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private db: PrismaService) {}
+  private readonly logger = new Logger(UserService.name, { timestamp: true });
+
+  async createUser(dataUser: Prisma.UserCreateInput): Promise<User> {
+    this.logger.log(`Creating user with email: ${dataUser.email}`);
+    return this.db.user.create({ data: dataUser });
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    this.logger.log('Fetching all users');
+    return this.db.user.findMany();
+  }
+
+  async editUser(id: string, dataUser: Prisma.UserUpdateInput): Promise<User> {
+    const numericId = Number(id);
+    try {
+      if (isNaN(numericId)) {
+        throw new Error('Invalid user id');
+      }
+
+      this.logger.log(`Editing user with id: ${numericId}`);
+
+      const user = await this.db.user.update({
+        where: { id: numericId },
+        data: dataUser,
+      });
+
+      return user;
+    } catch (error) {
+      this.logger.error(`Error updating user ${numericId}`, error);
+
+      throw new Error('User not found or update failed');
+    }
+  }
+}
